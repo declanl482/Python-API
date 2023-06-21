34915e57493ebaa77ad9f8f91045b3ac6afe5611
Declan <13dlin@gmail.com>
2023-06-20 20:11:35 -0400

    Dockerizing the api/postgresql database

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,5 +1,6 @@
 from fastapi import status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import  IntegrityError
 from .. import models, schemas, utils
 from ..database import get_db
 
@@ -18,10 +19,15 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     hashed_password = utils.hash(user.password)
     user.password = hashed_password
     new_user = models.User(**user.dict())
-    db.add(new_user) 
-    db.commit()
-    db.refresh(new_user)
-
+    
+    try:
+        db.add(new_user) 
+        db.commit()
+        db.refresh(new_user)
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=f"This email is already registered to another user.")
     return new_user
 
 # Read a user
